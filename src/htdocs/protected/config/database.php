359c370dca5cce2e94b6008de73c71f892bc518d
Valery Shvargonov <animekazna@gmail.com>
2019-12-23 18:45:52 +0800

    init commit

---
+++ src/htdocs/protected/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	'connectionString' => 'mysql:host=db;dbname=rms',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => 'qW1234',
+	'charset' => 'utf8',
+);
